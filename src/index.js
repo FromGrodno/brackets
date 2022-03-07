a1d3f42b7785d925f13b5c7945e79372531f1601
Hancharuk Aliaksei <hancharuk19257@gmail.com>
2022-03-07 09:25:38 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+   let openArr = [];
+   let closeArr = [];
+   bracketsConfig.forEach((elem) => {
+      openArr.push(elem[0]);
+      closeArr.push(elem[1])
+   })
+   for (let i = 0; i < str.length; i++) {
+      let bracket = str[i];
+      let topElement = stack[stack.length - 1];
+      if (openArr.includes(bracket) && !closeArr.includes(bracket)) {
+         stack.push(bracket);
+      }
+      else if (openArr.includes(bracket) && closeArr.includes(bracket) && !stack.includes(bracket)) {
+         stack.push(bracket);
+      }
+      else {
+         if (stack.length === 0) {
+            return false;
+         }
+         topElement = stack[stack.length - 1];
+         if (openArr[closeArr.indexOf(bracket)] === topElement) {
+            stack.pop();
+         } else {
+            return false;
+         }
+      }
+   }
+   return stack.length === 0;
 }
